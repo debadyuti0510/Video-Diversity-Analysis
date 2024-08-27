2f6f459bab54cbb5dcfb857cd5b228a2631e0640
debadyuti0510 <debadyuti4@gmail.com>
2024-08-27 20:24:10 +0100

    added ood experiments and timed facial analysis and pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,21 @@
 
 import sys
+import time
 from utils import setup_experiment, run_experiment
 from visualization_utils import generate_visualizations
 
 if __name__ == "__main__":
+    # Start timing
+    start_time = time.time()
     # Extract demography data
     setup_experiment()
     run_experiment(sys.argv[1])
+    # End time
+    end_time = time.time()
+    time_taken = (end_time - start_time) / 60 # In minutes
+    print(f"Took {time_taken:.3f} mins to run pipeline.")
     generate_visualizations()
+    
 
 
 
